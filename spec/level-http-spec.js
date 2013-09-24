ae77bf2c2ce76d513cbfd4bae0000e6cd424f97b
Matt Hernandez <matt@modulus.io>
2013-09-24 15:09:06 -0400

    Added tests to verify that value encoding is set based on Content-Type header.

--- spec/level-http-spec.js
+++ spec/level-http-spec.js
@@ -93,4 +93,178 @@ describe('level-http', function() {
       handler(req);
     });
   });
+
+  describe('database encoding', function() {
+
+    it('db#get as json encoding when content type is set to application/json', function(done) {
+      var handler = getHandler('/data/{key}', 'GET'), encoding;
+
+      spyOn(db, 'get').andCallFake(function() {
+        encoding = arguments[1].valueEncoding;
+        arguments[2]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('json');
+        expect(db.get).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = 'application/json';
+
+      handler(req);
+    });
+
+    it('db#put as json encoding when content type is set to application/json', function(done) {
+      var handler = getHandler('/data/{key}', 'POST'), encoding;
+
+      spyOn(db, 'put').andCallFake(function() {
+        encoding = arguments[2].valueEncoding;
+        arguments[3]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('json');
+        expect(db.put).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = 'application/json';
+
+      handler(req);
+    });
+
+    it('db#del as json encoding when content type is set to application/json', function(done) {
+      var handler = getHandler('/data/{key}', 'DELETE'), encoding;
+
+      spyOn(db, 'del').andCallFake(function() {
+        encoding = arguments[1].valueEncoding;
+        arguments[2]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('json');
+        expect(db.del).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = 'application/json';
+
+      handler(req);
+    });
+
+    it('db#get as utf8 encoding when content type is set to text/plain', function(done) {
+      var handler = getHandler('/data/{key}', 'GET'), encoding;
+
+      spyOn(db, 'get').andCallFake(function() {
+        encoding = arguments[1].valueEncoding;
+        arguments[2]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('utf8');
+        expect(db.get).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = 'text/plain';
+
+      handler(req);
+    });
+
+    it('db#put as utf8 encoding when content type is set to text/plain', function(done) {
+      var handler = getHandler('/data/{key}', 'POST'), encoding;
+
+      spyOn(db, 'put').andCallFake(function() {
+        encoding = arguments[2].valueEncoding;
+        arguments[3]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('utf8');
+        expect(db.put).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = 'text/plain';
+
+      handler(req);
+    });
+
+    it('db#del as utf8 encoding when content type is set to text/plain', function(done) {
+      var handler = getHandler('/data/{key}', 'DELETE'), encoding;
+
+      spyOn(db, 'del').andCallFake(function() {
+        encoding = arguments[1].valueEncoding;
+        arguments[2]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('utf8');
+        expect(db.del).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = 'text/plain';
+
+      handler(req);
+    });
+
+    it('db#get as utf8 encoding when content type is not set', function(done) {
+      var handler = getHandler('/data/{key}', 'GET'), encoding;
+
+      spyOn(db, 'get').andCallFake(function() {
+        encoding = arguments[1].valueEncoding;
+        arguments[2]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('utf8');
+        expect(db.get).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = '';
+
+      handler(req);
+    });
+
+    it('db#put as utf8 encoding when content type is not set', function(done) {
+      var handler = getHandler('/data/{key}', 'POST'), encoding;
+
+      spyOn(db, 'put').andCallFake(function() {
+        encoding = arguments[2].valueEncoding;
+        arguments[3]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('utf8');
+        expect(db.put).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = '';
+
+      handler(req);
+    });
+
+    it('db#del as utf8 encoding when content type is not set', function(done) {
+      var handler = getHandler('/data/{key}', 'DELETE'), encoding;
+
+      spyOn(db, 'del').andCallFake(function() {
+        encoding = arguments[1].valueEncoding;
+        arguments[2]();
+      });
+
+      req.reply = function() {
+        expect(encoding).toEqual('utf8');
+        expect(db.del).wasCalled();
+        done();
+      };
+
+      req.raw.req.headers['content-type'] = '';
+
+      handler(req);
+    });
+  });
 });
